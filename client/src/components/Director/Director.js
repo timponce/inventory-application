58d66dce15e59886ee7afb9f2aca1fe64d3f6a8a
Timothy Ponce <timothyjponce@gmail.com>
2022-03-02 18:45:50 -0800

    Add lifetime to directors

--- client/src/components/Director/Director.js
+++ client/src/components/Director/Director.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { format, parseISO } from "date-fns";
 
 export default function Director(props) {
+  function getLifetimeString(birth, death) {
+    let lifetime_string;
+    if (birth) {
+      lifetime_string = format(parseISO(birth), "MMMM do y");
+    }
+    lifetime_string += " - ";
+    if (death) {
+      lifetime_string += format(parseISO(death), "MMMM do y");
+    }
+    return lifetime_string;
+  }
+
   const directorCard = props.directorData.map((director) => (
     <div key={director._id}>
       <h3>{director.first_name + " " + director.last_name}</h3>
-      <p>{director.date_of_birth}</p>
+      <p>{getLifetimeString(director.date_of_birth, director.date_of_death)}</p>
     </div>
   ));
 
--- client/src/components/Film/Film.js
+++ client/src/components/Film/Film.js
@@ -3,11 +3,14 @@ import React from "react";
 export default function Film(props) {
   const filmCard = props.filmData.map((film) => (
     <div key={film._id}>
-      <h3>{film.title}</h3>
-      <p>{film.director.first_name + " " + film.director.last_name}</p>
-      <p>{film.release}</p>
-      <p>{film.summary}</p>
+      <h3>Film: {film.title}</h3>
       <p>
+        Director: {film.director.first_name + " " + film.director.last_name}
+      </p>
+      <p>Release Date: {film.release}</p>
+      <p>Summary: {film.summary}</p>
+      <p>
+        Genre:{" "}
         {film.genre.map((item, i) => (
           <span key={i}>{item.name} </span>
         ))}
