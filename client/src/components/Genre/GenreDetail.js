b69e9ca04f4f9589e72d9f90adbf83316d80e01b
Timothy Ponce <timothyjponce@gmail.com>
2022-03-15 12:42:16 -0700

    Add edit and delete button to genre detail page - Delete method in progress

--- client/src/components/Genre/GenreDetail.js
+++ client/src/components/Genre/GenreDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Header from "../Core/Header";
 import Footer from "../Core/Footer";
 import Loading from "../Core/Loading";
@@ -16,28 +16,116 @@ import {
   LinkOverlay,
   HStack,
   Tag,
+  Center,
+  Menu,
+  MenuButton,
+  IconButton,
+  MenuList,
+  MenuItem,
+  useDisclosure,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  Button,
 } from "@chakra-ui/react";
 import { format, parseISO } from "date-fns";
+import { BsPencilSquare } from "react-icons/bs";
 
 export default function GenreDetail() {
   const [genreDetailData, setGenreDetailData] = React.useState([]);
 
   const { id } = useParams();
+  const [isDelete, setIsDelete] = React.useState(false);
+  let apiUrl = isDelete ? `/api/genre/${id}/delete` : `/api/genre/${id}`;
 
   useEffect(() => {
-    fetch(`/api/genre/${id}`)
+    fetch(apiUrl)
       .then((res) => res.json())
       .then((data) => setGenreDetailData(data));
   }, []);
+
+  useEffect(() => {
+    setIsDelete(isOpen);
+  });
+
+  let navigate = useNavigate();
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await fetch(apiUrl, {
+      method: "POST",
+    }).then(() => navigate("/genres"));
+  }
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
 
   return (
     <Container maxW="1600px" p="0">
       <Header />
       {genreDetailData.length !== 0 ? (
         <Box mx={{ base: "10px", md: "60px" }}>
-          <Heading as="h1" size="4xl" textAlign="center" mb="20px">
-            {genreDetailData.genre.name}
-          </Heading>
+          <Center>
+            <Heading as="h1" size="4xl" textAlign="center" mb="20px">
+              {genreDetailData.genre.name}
+            </Heading>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Menu"
+                fontSize="16px"
+                size="sm"
+                variant="ghost"
+                alignSelf="start"
+                icon={<BsPencilSquare />}
+              ></MenuButton>
+              <MenuList>
+                <MenuItem as="a" href={`/genre/${id}/update`}>
+                  Edit
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    onOpen();
+                    setIsDelete(true);
+                  }}
+                >
+                  Delete
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Center>
+          <AlertDialog
+            isOpen={isOpen}
+            leastDestructiveRef={cancelRef}
+            onClose={onClose}
+            blockScrollOnMount={false}
+          >
+            <AlertDialogOverlay>
+              <form method="POST" action="" onSubmit={handleSubmit}>
+                <AlertDialogContent>
+                  <AlertDialogHeader>Delete Film</AlertDialogHeader>
+                  <AlertDialogBody>
+                    Are you sure? You can't undo this action afterwards.
+                  </AlertDialogBody>
+                  <AlertDialogFooter>
+                    <Button ref={cancelRef} onClick={onClose}>
+                      Cancel
+                    </Button>
+                    <Button
+                      type="submit"
+                      colorScheme="red"
+                      onClick={onClose}
+                      ml="3"
+                    >
+                      Delete
+                    </Button>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </form>
+            </AlertDialogOverlay>
+          </AlertDialog>
           <Grid
             templateColumns={{
               base: "repeat(2, 1fr)",
