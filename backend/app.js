76c8d7abbddf2b98af89c9caad1a2aad7409284e
Timothy Ponce <timothyjponce@gmail.com>
2022-03-22 15:08:15 -0700

    Correct path to client

--- backend/app.js
+++ backend/app.js
@@ -22,13 +22,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.use("/", indexRouter);
 app.use("/api", apiRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = app;
